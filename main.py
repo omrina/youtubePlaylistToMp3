bd4c0a0baaab9ba924358930a38ca7d6ec39fed4
omrina <omri.nathanson@gmail.com>
2022-02-24 19:37:31 +0200

    add gitignore add first version of main.py

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from pytube import Playlist
 from moviepy.editor import *
 
 
+'''set to True when using this script through cmd/bash'''
+is_run_with_cmd = False
+
+
 def ensure_url_arg():
     if len(sys.argv) != 2:
         print('need a playlist url as 2nd argument')
         exit(1)
 
 
-# ensureUrlArg()
-# playlist_url = sys.argv[1]
-
+if is_run_with_cmd:
+    ensure_url_arg()
+    playlist_url = sys.argv[1]
+else:
+    playlist_url = 'https://www.youtube.com/playlist?list=PLAv3wwZw7pE2-CvBZZUe2sCu0p50dPUCW'
+    
 mp3_output_folder = './mp3_songs'
-playlist_url = 'https://www.youtube.com/playlist?list=PLAv3wwZw7pE2-CvBZZUe2sCu0p50dPUCW'
 Path(mp3_output_folder).mkdir(parents=True, exist_ok=True)
 
 playlist = Playlist(playlist_url)
@@ -24,11 +30,3 @@ print(F'downloading: {playlist.title}')
 for video in playlist.videos:
     print(F'downloading: {video.title}')
     video.streams.filter(only_audio=True).first().download(output_path=mp3_output_folder, filename=F"{video.title}.mp3")
-
-
-'''
-1 - pip uninstall -y numpy
-2 - pip uninstall -y setuptools
-3 - pip install setuptools
-4 - pip install numpy
-'''
